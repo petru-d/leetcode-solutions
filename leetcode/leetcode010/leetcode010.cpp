1a6b55061e119379cc44ca05f2ae129ef1d3491b
petru-d <berserk.ro@gmail.com>
2017-12-05 13:54:37 +0100

    added DP solution for p010 (from leetcode)

--- leetcode/leetcode010/leetcode010.cpp
+++ leetcode/leetcode010/leetcode010.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -100,28 +101,59 @@ namespace Solution
     }
 
     //dissapointing naive solution :(
-    bool isMatch_naive(string s, string p)
+    bool isMatch_naive(const string& s, const string& p)
     {
         return isMatch_naive(s, 0, p, 0);
     }
 
+    // Dynamic Programming solution (from leetcode)
+    bool isMatch_dp(const string& s, const string& p) 
+    {
+        /**
+        * f[i][j]: if s[0..i-1] matches p[0..j-1]
+        * if p[j - 1] != '*'
+        *      f[i][j] = f[i - 1][j - 1] && s[i - 1] == p[j - 1]
+        * if p[j - 1] == '*', denote p[j - 2] with x
+        *      f[i][j] is true iff any of the following is true
+        *      1) "x*" repeats 0 time and matches empty: f[i][j - 2]
+        *      2) "x*" repeats >= 1 times and matches "x*x": s[i - 1] == x && f[i - 1][j]
+        * '.' matches any single character
+        */
+        int m = int(s.size()), n = int(p.size());
+        vector<vector<bool>> f(m + 1, vector<bool>(n + 1, false));
+
+        f[0][0] = true;
+        for (int j = 1; j <= n; j++)
+            f[0][j] = j > 1 && '*' == p[j - 1] && f[0][j - 2];
+
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (p[j - 1] != '*')
+                    f[i][j] = f[i - 1][j - 1] && (s[i - 1] == p[j - 1] || '.' == p[j - 1]);
+                else
+                    // p[0] cannot be '*' so no need to check "j > 1" here
+                    f[i][j] = f[i][j - 2] || (s[i - 1] == p[j - 2] || '.' == p[j - 2]) && f[i - 1][j];
+
+        return f[m][n];
+    }
+
     bool isMatch(string s, string p)
     {
-        
+        return isMatch_dp(s, p);
     }
 }
 
 int main()
 {
-    cout << Solution::isMatch_naive("aa", "a") << "\n";
-    cout << Solution::isMatch_naive("aa", "aa") << "\n";
-    cout << Solution::isMatch_naive("aaa", "aa") << "\n";
-    cout << Solution::isMatch_naive("aa", "a*") << "\n";
-    cout << Solution::isMatch_naive("aa", ".*") << "\n";
-    cout << Solution::isMatch_naive("ab", ".*") << "\n";
-    cout << Solution::isMatch_naive("aab", "c*a*b") << "\n";
-    cout << Solution::isMatch_naive("a", "ab*") << "\n";
-    cout << Solution::isMatch_naive("acaabbaccbbacaabbbb", "a*.*b*.*a*aa*a*") << "\n";
+    cout << Solution::isMatch("aa", "a") << "\n";
+    cout << Solution::isMatch("aa", "aa") << "\n";
+    cout << Solution::isMatch("aaa", "aa") << "\n";
+    cout << Solution::isMatch("aa", "a*") << "\n";
+    cout << Solution::isMatch("aa", ".*") << "\n";
+    cout << Solution::isMatch("ab", ".*") << "\n";
+    cout << Solution::isMatch("aab", "c*a*b") << "\n";
+    cout << Solution::isMatch("a", "ab*") << "\n";
+    cout << Solution::isMatch("acaabbaccbbacaabbbb", "a*.*b*.*a*aa*a*") << "\n";
     return 0;
 }
 
