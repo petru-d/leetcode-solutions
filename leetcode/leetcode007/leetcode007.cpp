b5617f9d91c12ed54c855782484ad1652d012cc5
petru-d <berserk.ro@gmail.com>
2017-12-15 14:23:23 +0100

    solution for p007

---
+++ leetcode/leetcode007/leetcode007.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+namespace Solution
+{
+    int reverse(int x)
+    {
+        auto negative(x < 0);
+
+        if (negative) 
+            x = -x;
+
+        int result = 0;
+        while (0 != x)
+        {
+            auto r = std::div(x, 10);
+
+            //Test for overflow (also handles min int).
+            if (result > (2147483647 - r.rem) / 10) return 0;
+
+            result = 10 * result + r.rem;
+            x = r.quot;
+        }
+
+        return negative ? -result : result;
+    }
+}
+
+int main()
+{
+    cout << Solution::reverse(numeric_limits<int>::min());
+    return 0;
+}
+
